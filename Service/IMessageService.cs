2ec02f2ca71d4240264f2247df24a26c93946e9c
satoru-naruko <naruko505@gmail.com>
2020-08-31 23:21:06 +0900

    IMessageService IFを追加

---
+++ Service/IMessageService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrismSample.Service
+{
+    public interface IMessageService
+    {
+        void ShowDialog(string message);
+        MessageBoxResult Question(string message);
+    }
+}
