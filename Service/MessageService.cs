bce37ddd4d287df54cd7dc5da2ffa0c6fe72c197
SatoruNaruko <naruko505@gmail.com>
2020-09-06 11:58:11 +0900

    MessageServiceを追加

---
+++ Service/MessageService.cs
@@ -0,0 +1,27 @@
+﻿using PrismSample.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace PrismSample.Service
+{
+    public sealed class MessageService : IMessageService
+    {
+        public MessageBoxResult Question(string message)
+        {
+            return MessageBox.Show(
+                message,
+                "問い合わせ",
+                MessageBoxButton.OKCancel,
+                 MessageBoxImage.Question);
+        }
+
+        public void ShowDialog(string message)
+        {
+            MessageBox.Show(message);
+        }
+    }
+}
